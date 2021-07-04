bc253f9e2d48db99e7952f1c1c3698ef767987f6
Keith Smith <keithbsmith87@gmail.com>
2021-07-04 17:40:55 -0400

    additional pwa meta tags

--- pages/hidden/uneeq/index.jsx
+++ pages/hidden/uneeq/index.jsx
@@ -4,7 +4,9 @@ const Index = () => {
   return(<>
     <Head>
       <script type="text/javascript" src="https://hosted.us.uneeq.io/deploy/75ed7a37-19ca-4c68-96e3-fbc074e0517a" async defer></script>
+      <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
       <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"></meta>
+      <link rel="manifest" href="/manifest.json" />
     </Head>
   </>)
 }
