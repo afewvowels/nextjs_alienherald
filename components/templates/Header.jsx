0d1fadd81d2c383add38541be739c54a79beed54
Keith Smith <keithbsmith87@gmail.com>
2021-07-14 13:49:22 -0400

    update header to include warehouse link

--- components/templates/Header.jsx
+++ components/templates/Header.jsx
@@ -5,10 +5,12 @@ import styles from 'styles/templates.module.css'
 const Header = () => {
   return(<>
     <header className={styles.header}>
-      <h1 className={styles.headerTitle}>A<span>H</span></h1>
+      <Link href='/' passHref>
+        <h1 className={styles.headerTitle}>A<span>H</span></h1>
+      </Link>
       <nav className={styles.headerNav}>
         <ul>
-          <li><Link href='/' passHref>Home</Link></li>
+          <li><Link href='https://warehouse.alienherald.earth/' passHref>Warehouse</Link></li>
           <li><Link href='/project' passHref>Projects</Link></li>
           <li><Link href='https://alienherald.threadless.com/designs/alienherald' passHref>Merch</Link></li>
         </ul>
