304e2285aaccc726216ae91cfcd9d2c4fb5b1cda
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2021-08-02 19:53:27 -0400

    one more change

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -23,7 +23,7 @@ class MyDocument extends Document {
 
           <meta name="apple-mobile-web-app-title" content="AlienHerald"/>
           <meta name="apple-mobile-web-app-capable" content="yes"/>
-          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
+          <meta name="apple-mobile-web-app-status-bar-style" content="default"/>
 
           <meta name="application-name" content="AlienHerald"/>
 
