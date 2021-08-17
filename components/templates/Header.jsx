5d684c1d5a4ff188c450b8bdabf13104c633eed9
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2021-08-17 08:05:45 -0400

    push hambuger menu

--- components/templates/Header.jsx
+++ components/templates/Header.jsx
@@ -1,8 +1,39 @@
 import Link from 'next/link'
+import { useState } from 'react'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/pro-solid-svg-icons'
 
 import styles from 'styles/templates.module.css'
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const hideResponsiveNav = () => setIsOpen(false)
+  const showResponsiveNav = () => setIsOpen(true)
+
+  let responsiveNav
+  if (isOpen) {
+    responsiveNav =
+    <span>
+      <div className={styles.responsiveIcon} onClick={hideResponsiveNav}>
+        <FontAwesomeIcon icon={faBars} />
+      </div>
+      <nav id='responsiveHeaderNavId' className={styles.responsiveHeaderNav}>
+        <ul>
+          <li><Link href='https://warehouse.alienherald.earth/' passHref>Warehouse</Link></li>
+          <li><Link href='/project' passHref>Projects</Link></li>
+          <li><Link href='https://alienherald.threadless.com/designs/alienherald' passHref>Merch</Link></li>
+        </ul>
+      </nav>
+    </span>
+  } else {
+    responsiveNav =
+    <div className={styles.responsiveIcon} onClick={showResponsiveNav}>
+      <FontAwesomeIcon icon={faBars} />
+    </div>
+  }
+
   return(<>
     <header className={styles.header}>
       <Link href='/' passHref>
@@ -15,6 +46,7 @@ const Header = () => {
           <li><Link href='https://alienherald.threadless.com/designs/alienherald' passHref>Merch</Link></li>
         </ul>
       </nav>
+      {responsiveNav}
     </header>
   </>)
 }
