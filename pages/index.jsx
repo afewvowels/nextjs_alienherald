4ac5e659532ae8f890718f81cc7827e7b30384d3
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2021-09-22 18:39:26 -0400

    push style update

--- pages/index.jsx
+++ pages/index.jsx
@@ -19,44 +19,6 @@ const Home = () => {
     <div className={styles.container}>
       <Image src='/logo.svg' alt="AlienHerald logo" height={512} width={512} className={styles.logo} />
       <section className={styles.linkIcons}>
-        {/* <span>
-          <Link href="https://www.facebook.com/AlienHerald" passHref>
-            <FontAwesomeIcon icon={faFacebookF} />
-          </Link>
-          <Link href="https://www.github.com/AlienHerald" passHref>
-            <FontAwesomeIcon icon={faGithub} />
-          </Link>
-          <Link href="https://www.instagram.com/alienheraldworkshop" passHref>
-            <FontAwesomeIcon icon={faInstagram} />
-          </Link>
-          <Link href="https://www.patreon.com/AlienHerald" passHref>
-            <FontAwesomeIcon icon={faPatreon} />
-          </Link>
-          <Link href="https://www.twitch.tv/alienherald" passHref>
-            <FontAwesomeIcon icon={faTwitch} />
-          </Link>
-          <Link href="https://alienherald.threadless.com/designs/alienherald" passHref>
-            <FontAwesomeIcon icon={faTshirt} />
-          </Link>
-        </span>
-        <span>
-          <Link href="https://www.tiktok.com/@alienherald" passHref>
-            <FontAwesomeIcon icon={faTiktok} />
-          </Link>
-          <Link href="https://thingiverse.com/AlienHerald" passHref>
-            <FontAwesomeIcon icon={faToolbox} />
-          </Link>
-          <Link href="https://twitter.com/AlienHerald" passHref>
-            <FontAwesomeIcon icon={faTwitter} />
-          </Link>
-          <Link href="https://paypal.me/AlienHerald" passHref>
-            <FontAwesomeIcon icon={faPaypal} />
-          </Link>
-          <Link href="https://vimeo.com/user144700268" passHref>
-            <FontAwesomeIcon icon={faVimeo} />
-          </Link>
-        </span> */}
-
         <Link href="https://alienherald.threadless.com/designs/alienherald" passHref>
           <div className={styles.socialLink}>
             <FontAwesomeIcon icon={faTshirt} />
